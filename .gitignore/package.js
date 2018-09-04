351a90a2b6d32f6b540c7a577be7cff5e8c2c7dd
onizuka93 <42942677+onizuka93@users.noreply.github.com>
2018-09-04 22:22:07 +0200

    Update package.js

--- .gitignore/package.js
+++ .gitignore/package.js
@@ -1,4 +1,15 @@
 {
-  "name": "Discod Bot",
-  "version": ""
-}
+  "name": "Discord Bot",
+  "version": "1.0.0",
+  "description": "",
+  "main": "main.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "author": "Onizuka93",
+  "license": "ISC",
+  "dependencies": {
+    "discord.js": "^11.3.2"
+  }
+} 
+
